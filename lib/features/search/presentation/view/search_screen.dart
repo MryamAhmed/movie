72a1624a3cc0bafd0bd56040b2d3641a3c66129c
MryamAhmed <mrym8499@gmail.com>
2024-07-02 00:29:30 +0300

    solve search cubit bad staate issue

--- lib/features/search/presentation/view/search_screen.dart
+++ lib/features/search/presentation/view/search_screen.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies/features/search/presentation/view/widget/search_body.dart';
 
 import '../../../../core/utils/service_locator.dart';
 import '../manger/search_cubit.dart';
-import 'widget/search_body.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) {
-        return getIt<SearchMovieCubit>();
-      },
+    return BlocProvider.value(
+      value: getIt<SearchMovieCubit>(),
       child: const SearchBody(),
     );
+
+    // return BlocProvider(
+    //   create: (BuildContext context) {
+    //     return getIt<SearchMovieCubit>();
+    //   },
+    //   child: const SearchBody(),
+    // );
   }
 }
